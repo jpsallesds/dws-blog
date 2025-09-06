df1bc8e61b1a5bf006048fae8926c04448c5b27e
João Pedro Salles <joaopedro.salles@dentsu.com>
2025-09-06 01:08:19 -0300

    feat: add dropdown component

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,75 @@
+import { useState, useRef, useEffect } from 'react';
+import { FaAngleDown } from 'react-icons/fa6';
+import styles from './Dropdown.module.scss';
+
+interface DropdownProps {
+    options: string[];
+    placeholder?: string;
+}
+
+const Dropdown = ({ options, placeholder = "Category" }: DropdownProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [selected, setSelected] = useState<string[]>([]);
+    const dropdownRef = useRef<HTMLDivElement>(null);
+
+    const handleSelect = (option: string) => {
+        setSelected(prev => {
+            const exists = prev.includes(option);
+            const newSelected = exists ? prev.filter(item => item !== option) : [...prev, option];
+            return newSelected;
+        });
+    };
+
+    const handleReset = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        setSelected([]);
+    };
+
+    useEffect(() => {
+  const handleClickOutside = (e: PointerEvent) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(e.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  document.addEventListener("pointerdown", handleClickOutside);
+  return () => document.removeEventListener("pointerdown", handleClickOutside);
+}, []);
+
+
+    return (
+        <div className={styles.dropdown} ref={dropdownRef}>
+            <button type="button" className={styles.toggle} onClick={() => setIsOpen(!isOpen)}>
+                {selected.length > 0 ? selected.join(', ') : placeholder}
+
+                {selected.length > 0 ? (
+                    <span className={styles.icon} onClick={handleReset}>
+                        ✕
+                    </span>
+                ) : (
+                    <span className={styles.icon}>
+                        <FaAngleDown size={12} />
+                    </span>
+                )}
+            </button>
+
+            {isOpen && (
+                <div className={styles.menuContainer}>
+                    <ul className={styles.menu}>
+                        {options.map(option => (
+                            <li
+                                key={option}
+                                className={styles.option}
+                                onClick={() => handleSelect(option)}
+                            >
+                                {option}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+}
+
+export default Dropdown;
