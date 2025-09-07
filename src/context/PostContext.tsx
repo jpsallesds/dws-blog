5ae317e1134947c5ba1ab9ac9cb4e57406660d84
João Pedro Salles <joaopedro.salles@dentsu.com>
2025-09-07 19:10:12 -0300

    chore: add post context

---
+++ src/context/PostContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, useContext, useState, type ReactNode } from "react";
+import type { Post } from "src/types/Post";
+
+type PostsContextType = {
+  posts: Post[];
+  latestPosts: Post[];
+  setContextPosts: React.Dispatch<React.SetStateAction<Post[]>>;
+};
+
+const PostsContext = createContext<PostsContextType | undefined>(undefined);
+
+export const PostsProvider = ({ children }: { children: ReactNode }) => {
+  const [posts, setContextPosts] = useState<Post[]>([]); 
+
+  const latestPosts = [...posts]
+    .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime())
+    .slice(0, 4);
+
+  return (
+    <PostsContext.Provider value={{ posts, latestPosts, setContextPosts }}>
+      {children}
+    </PostsContext.Provider>
+  );
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const usePosts = () => {
+  const context = useContext(PostsContext);
+  if (!context) {
+    throw new Error("usePosts deve ser usado dentro de um PostsProvider");
+  }
+  return context;
+};
